375cfba6b4f0c7cfc8219a919c5bbc8045c0ba82
Parsa Hajiha <hajihaparsa2004@gmail.com>
2023-02-26 15:12:44 -0600

    projects created

--- acme-website/src/App.js
+++ acme-website/src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Home />
+      <Projects />
     </div>
   );
 }
--- acme-website/src/components/projects.js
+++ acme-website/src/components/projects.js
@@ -1,4 +1,43 @@
 import "./projects.css";
 
+function Projects() {
+    return(
+        <>
+            <div className="projects">
+                <h1 id="paragraph_header">Projects</h1>
+            </div>
 
+            <div className="card_wrapper">
+              <div className="card">
+                  <div className="card_body">
+                      <img class="card_img" src="images/minerwarehouse_logo.png"/>
+                      <h2 className="card_title">MinerWarehouse</h2>
+                      <p className="card_description">E-Commerce website</p>
+                      <button className="card_btn">Learn more</button>
+                  </div>
+              </div>
+            
 
+            
+              <div className="card">
+                  <div className="card_body">
+                      <img class="card_img" src="images/acme_logo.png"/>
+                      <h2 className="card_title">ACM-E Website</h2>
+                      <p className="card_description">ACM-E website</p>
+                      <button className="card_btn">Learn more</button>
+                  </div>
+              </div>
+
+              <div className="card">
+                  <div className="card_body">
+                      <img class="card_img" src="images/acme_logo.png"/>
+                      <h2 className="card_title">Coming Soon...</h2>
+                  </div>
+              </div>
+
+            </div>
+        </>
+    );
+}
+
+export default Projects;
