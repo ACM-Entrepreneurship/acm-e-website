695652bc663c696670aff633fe2a46447e39ec14
Parsa Hajiha <hajihaparsa2004@gmail.com>
2023-02-25 22:51:29 -0600

    home page created

--- acme-website/src/App.js
+++ acme-website/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home />
     </div>
   );
 }
---
+++ acme-website/src/components/home.js
@@ -0,0 +1,23 @@
+import "./home.css";
+
+function Home (){
+    return(
+        <>
+          <div className="header">
+            <img src="images/header.jpg" id="header_img"/>
+          </div>
+
+          <div className="paragraph">
+            <div className="header">
+              <h1 id="paragraph_header">Vision</h1>
+            </div>
+
+            <div className="text">
+              <p id="paragraph_text">ACM Entrepreneurship is an organization that focuses on two main goals. The first is to provide exposure to students from any majors through hands-on learning in their fields of interest with our virtual companies and our external projects. The second goal is to foster entrepreneurial mindset by allowing students to come up with innovative ideas for our virtual companies, by providing support for students' startups. We welcome everyone and help our members build the technical as well as the soft skills that are sought in the professional world.</p>
+            </div>
+          </div>
+        </>
+    )
+}
+
+export default Home;
