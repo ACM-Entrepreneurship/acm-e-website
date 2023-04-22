6785eda50aee8368f6bd7adb84cea0e59c83ef6e
Parsa Hajiha <hajihaparsa2004@gmail.com>
2023-04-22 14:11:07 -0500

    team section updated

--- acme-website/src/components/carousel.js
+++ acme-website/src/components/carousel.js
@@ -2,9 +2,79 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "./carousel.css";
 
+class Card extends Component {
+  render() {
+    const { imgSrc, name, role, twitterLink, linkedInLink, githubLink, emailLink } = this.props.cardData;
+    return (
+      <div className="card_section">
+        <div className="row_team">
+          <div className="column_team">
+            <div className="card_team">
+              <div className="img_container_team">
+                <img src={imgSrc} alt={name} />
+              </div>
+              <h3>{name}</h3>
+              <p>{role}</p>
+              <div className="icons_team">
+
+                <a href={linkedInLink}>
+                  <i className="fab fa-linkedin"></i>
+                </a>
+                <a href={githubLink}>
+                  <i className="fab fa-github"></i>
+                </a>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 export default class Carousel extends Component {
   render() {
-    var settings = {
+    const cardDataList = [
+      {
+        imgSrc: "images/parsa_headshot.jpg",
+        name: "Luna Turner",
+        role: "Founder",
+        linkedInLink: "#",
+        githubLink: "#",
+      },
+      {
+        imgSrc: "images/parsa_headshot.jpg",
+        name: "Luna Turner",
+        role: "Founder",
+        linkedInLink: "#",
+        githubLink: "#",
+      },
+      {
+        imgSrc: "images/parsa_headshot.jpg",
+        name: "Luna Turner",
+        role: "Founder",
+        linkedInLink: "#",
+        githubLink: "#",
+      },
+      {
+        imgSrc: "images/parsa_headshot.jpg",
+        name: "Luna Turner",
+        role: "Founder",
+        linkedInLink: "#",
+        githubLink: "#",
+      },
+      {
+        imgSrc: "images/parsa_headshot.jpg",
+        name: "Luna Turner",
+        role: "Founder",
+        linkedInLink: "#",
+        githubLink: "#",
+      },
+      // add more card data objects as needed
+    ];
+
+    const settings = {
       dots: true,
       infinite: false,
       speed: 500,
@@ -38,43 +108,16 @@ export default class Carousel extends Component {
         }
       ]
     };
+
     return (
       <div id="team" className="carousel">
         <div className="projects">
           <h1 id="paragraph_header">Team</h1>
         </div>
         <Slider {...settings}>
-           <div className="carousel_card">
-             <img src="images/website-card.png" id="team_cards"/>
-           </div>
- 
-           <div className="carousel_card">
-             <img src="images/website-card.png" id="team_cards"/>
-           </div>
- 
-           <div className="carousel_card">
-             <img src="images/website-card.png" id="team_cards"/>
-           </div>
- 
-           <div className="carousel_card">
-             <img src="images/website-card.png" id="team_cards"/>
-           </div>
- 
-           <div className="carousel_card">
-             <img src="images/website-card.png" id="team_cards"/>
-           </div>
- 
-           <div className="carousel_card">
-             <img src="images/website-card.png" id="team_cards"/>
-           </div>
- 
-           <div className="carousel_card">
-             <img src="images/website-card.png" id="team_cards"/>
-           </div>
- 
-           <div className="carousel_card">
-             <img src="images/website-card.png" id="team_cards"/>
-           </div>
+          {cardDataList.map((cardData, index) => (
+            <Card key={index} cardData={cardData} />
+          ))}
         </Slider>
       </div>
     );
