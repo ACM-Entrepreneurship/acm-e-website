090c95145b80a79b20491b2f97614d6996b9fcb8
Parsa Hajiha <hajihaparsa2004@gmail.com>
2023-08-12 13:29:24 -0500

    404 test

--- acme-website/src/pages/page404.js
+++ acme-website/src/pages/page404.js
@@ -6,7 +6,7 @@ function Page404() {
     return(
         <>
         <div className='PageNotFound'>
-           <h1>404 Page Not Found</h1>
+           <h1>404 Page Not Found test</h1>
            <p>The page you are looking for does not exist.</p>
            <Link to="/"><button className='button-404'>Return Home</button></Link>
         </div>
