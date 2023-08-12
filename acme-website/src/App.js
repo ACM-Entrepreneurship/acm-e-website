bf9b9db0920b801be3371b991c06a8c3811a33d6
Parsa Hajiha <hajihaparsa2004@gmail.com>
2023-08-12 14:06:03 -0500

    404 added

--- acme-website/src/App.js
+++ acme-website/src/App.js
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import MinerWarehouse from "./pages/miner-wh.js";
 import Podcast from "./pages/podcast.js";
 import Main from './pages/main';
+import Page404 from './pages/page404';
 
 
 function App () {
@@ -15,6 +16,7 @@ function App () {
            <Route path="/" element={<Main />} />
            <Route path="/minerwarehouse" element={<MinerWarehouse />} />
            <Route path="/podcast" element={<Podcast />} />
+           <Route path="/*" element={<Page404 />} />
         </Routes>
        </div>
      </>
